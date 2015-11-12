019218e88c3789467a8add1d54b6048e033758a2
Jürgen Viljaste <j.viljaste@gmail.com>
2015-11-12 19:17:36 +0200

    Update

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+var current_version = '0.1.0';
+var new_version = '0.1.0';
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-replace');
+
+  grunt.initConfig({
+    replace: {
+      task1: {
+        options: {
+          patterns: [
+            {
+              match: current_version,
+              replacement: new_version
+            }
+          ],
+          usePrefix: false
+        },
+        files: [
+          {
+            expand: true,
+            src: [
+              'VERSION.md'
+            ]
+          }
+        ]
+      },
+      task2: {
+        options: {
+          patterns: [
+            {
+              match: '"version": "' + current_version + '"',
+              replacement: '"version": "' + new_version + '"'
+            }
+          ],
+          usePrefix: false
+        },
+        files: [
+          {
+            expand: true,
+            src: [
+              'package.json'
+            ]
+          }
+        ]
+      }
+    }
+  });
+
+  grunt.registerTask('bump', 'replace');
+  grunt.registerTask('default', 'replace');
+};
--- Vagrantfile
+++ Vagrantfile
@@ -35,7 +35,7 @@ Vagrant.configure("2") do |config|
       sudo apt-get update
       sudo apt-get upgrade -y
 
-      curl -ssl https://get.docker.com/ | sh
+      wget -qO- https://get.docker.com/ | sh
 
       sudo usermod -aG docker vagrant
 
